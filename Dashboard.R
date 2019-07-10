e4ec3bf0348547012ad18c75a0641f11840198ce
Hong <hxyue1@gmail.com>
2019-07-10 16:10:40 +1000

    Action button

--- Dashboard.R
+++ Dashboard.R
@@ -12,6 +12,7 @@ ui <-fluidPage(
     condition = "input.freq == 'daily'",
     dateRangeInput("range","Date Range:")
   ),
+  actionButton("launch","Go!"),
   
   mainPanel(
             plotOutput("price"),
@@ -22,27 +23,29 @@ ui <-fluidPage(
 
 server <- function(input,output) {
   
-  output$price <-renderPlot({
-    ticker <- input$ticker
-    av_api_key("RUDBPNS133OPNKY1")
-    
-    
-    if(input$freq == "intraday"){
-      data <- data.frame(av_get(symbol=ticker,av_fun="TIME_SERIES_INTRADAY",interval="5min"))
-      today <- Sys.Date()
-      time <- data$timestamp + 14400
-      attr(time,"tzone") <- "Australia/Sydney"
-      data$timestamp <- time
-      data <- data[c(as.Date(Sys.time()) == as.Date(time)),]
-    }
-    else{
-      data <- data.frame(av_get(symbol=ticker,av_fun="TIME_SERIES_DAILY"),outputsize="full")
-      data <- data[c(data$close!=0),]
-    }
-    
-
-    ggplot(data=data, aes(x=timestamp,y=close)) + geom_line() + theme_bw()
+  observeEvent(input$launch,{
+    output$price <-renderPlot({
+      ticker <- input$ticker
+      av_api_key("RUDBPNS133OPNKY1")
+      
+      
+      if(input$freq == "intraday"){
+        data <- data.frame(av_get(symbol=ticker,av_fun="TIME_SERIES_INTRADAY",interval="5min"))
+        today <- Sys.Date()
+        time <- data$timestamp + 14400
+        attr(time,"tzone") <- "Australia/Sydney"
+        data$timestamp <- time
+        data <- data[c(as.Date(Sys.time()) == as.Date(time)),]
+      }
+      else{
+        data <- data.frame(av_get(symbol=ticker,av_fun="TIME_SERIES_DAILY"),outputsize="full")
+        data <- data[c(data$close!=0),]
+      }
+      
+      ggplot(data=data, aes(x=timestamp,y=close)) + geom_line() + theme_bw()
+    })
   })
+  
   
   # output$mytable = DT::renderDataTable({
   #   ticker <- input$ticker
