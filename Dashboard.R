137a6a04df37d0344e1fabab81dd81080ac7efc4
Hong <hxyue1@gmail.com>
2019-07-10 14:27:27 +1000

    corrected timezone from UTC to EST

--- Dashboard.R
+++ Dashboard.R
@@ -17,14 +17,19 @@ server <- function(input,output) {
     
     ticker <- input$ticker
     av_api_key("RUDBPNS133OPNKY1")
-    data <- data.frame(av_get(symbol=ticker,av_fun="TIME_SERIES_DAILY"))
-    ggplot(data=data, aes(x=timestamp,y=close)) + geom_line()
+    today <- Sys.Date()
+    data <- data.frame(av_get(symbol=ticker,av_fun="TIME_SERIES_INTRADAY",interval="5min"))
+    time <- data$timestamp + 14400
+    attr(time,"tzone") <- "Australia/Sydney"
+    data$timestamp <- time
+    data <- data[c(as.Date(Sys.time()) == as.Date(time)),]
+    ggplot(data=data, aes(x=timestamp,y=open)) + geom_line()
   })
   
-  output$mytable = DT::renderDataTable({
-    ticker <- input$ticker
-    av_api_key("RUDBPNS133OPNKY1")
-    data <- data.frame(av_get(symbol=ticker,av_fun="TIME_SERIES_DAILY"))
-  })
+  # output$mytable = DT::renderDataTable({
+  #   ticker <- input$ticker
+  #   av_api_key("RUDBPNS133OPNKY1")
+  #   data <- data.frame(av_get(symbol=ticker,av_fun="TIME_SERIES_DAILY"))
+  # })
 }
 shinyApp(ui,server)
