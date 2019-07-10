a2627d48bae51dcdcde29f4aa7ec051faf4e696d
Hong <hxyue1@gmail.com>
2019-07-10 16:05:18 +1000

    daily and intraday options

--- Dashboard.R
+++ Dashboard.R
@@ -4,6 +4,14 @@ library(ggplot2)
 
 ui <-fluidPage(
   textInput("ticker","Asset Ticker"),
+  checkboxInput("smooth", "Smooth"),
+  selectInput("freq",h3("Frequency"),
+              choices = list("Intraday"= "intraday", "Daily"="daily")),
+  
+  conditionalPanel(
+    condition = "input.freq == 'daily'",
+    dateRangeInput("range","Date Range:")
+  ),
   
   mainPanel(
             plotOutput("price"),
@@ -13,17 +21,27 @@ ui <-fluidPage(
 )
 
 server <- function(input,output) {
+  
   output$price <-renderPlot({
-    
     ticker <- input$ticker
     av_api_key("RUDBPNS133OPNKY1")
-    today <- Sys.Date()
-    data <- data.frame(av_get(symbol=ticker,av_fun="TIME_SERIES_INTRADAY",interval="5min"))
-    time <- data$timestamp + 14400
-    attr(time,"tzone") <- "Australia/Sydney"
-    data$timestamp <- time
-    data <- data[c(as.Date(Sys.time()) == as.Date(time)),]
-    ggplot(data=data, aes(x=timestamp,y=open)) + geom_line()
+    
+    
+    if(input$freq == "intraday"){
+      data <- data.frame(av_get(symbol=ticker,av_fun="TIME_SERIES_INTRADAY",interval="5min"))
+      today <- Sys.Date()
+      time <- data$timestamp + 14400
+      attr(time,"tzone") <- "Australia/Sydney"
+      data$timestamp <- time
+      data <- data[c(as.Date(Sys.time()) == as.Date(time)),]
+    }
+    else{
+      data <- data.frame(av_get(symbol=ticker,av_fun="TIME_SERIES_DAILY"),outputsize="full")
+      data <- data[c(data$close!=0),]
+    }
+    
+
+    ggplot(data=data, aes(x=timestamp,y=close)) + geom_line() + theme_bw()
   })
   
   # output$mytable = DT::renderDataTable({
